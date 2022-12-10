14824b636c8103130f8a08c68d7556ddfff161b1
hayato0311 <hayato8088@gmail.com>
2022-12-10 22:16:06 +0900

    first commit

---
+++ android/app/src/main/kotlin/com/example/reliet/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.reliet
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/app.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+import './views/screens/home_screen.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomeScreen(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
