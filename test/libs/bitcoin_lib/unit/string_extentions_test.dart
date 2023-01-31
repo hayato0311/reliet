c814c5bbdd9ef1bd72514d8a26030d7343d6c3d8
hayato0311 <hayato8088@gmail.com>
2023-01-31 09:42:07 +0900

    add int and string extensions

---
+++ test/libs/bitcoin_lib/unit/string_extentions_test.dart
@@ -0,0 +1,18 @@
+import 'dart:convert';
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:reliet/libs/bitcoin_lib/lib/src/extensions/string_extensions.dart';
+
+void main() {
+  group('convert string into bytes', () {
+    test('without specified length', () {
+      expect('valid'.toBytes(), utf8.encode('valid'));
+    });
+    test('with valid length', () {
+      expect('valid'.toBytes(10), [...utf8.encode('valid'), 0, 0, 0, 0, 0]);
+    });
+    test('with invalid length', () {
+      expect(() => 'invalid'.toBytes(1), throwsArgumentError);
+    });
+  });
+}
