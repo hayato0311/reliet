c48eba39d67987110271b767f80f7c1d7e123d7f
hayato0311 <hayato8088@gmail.com>
2023-02-02 21:37:56 +0900

    add deserialize function to Magic

--- lib/libs/bitcoin_lib/lib/src/protocol/types/magic.dart
+++ lib/libs/bitcoin_lib/lib/src/protocol/types/magic.dart
@@ -7,6 +7,22 @@ enum Magic {
   testnet(0x0709110b);
 
   const Magic(this.value);
+
+  factory Magic.deserialize(Uint8List bytes) {
+    final magicValue = CreateInt.fromUint32leBytes(bytes);
+
+    if (magicValue != Magic.mainnet.value &&
+        magicValue != Magic.testnet.value) {
+      throw ArgumentError();
+    }
+
+    if (magicValue == Magic.mainnet.value) {
+      return Magic.mainnet;
+    }
+
+    return Magic.testnet;
+  }
+
   final int value;
 
   Uint8List serialize() => value.toUint32leBytes();
