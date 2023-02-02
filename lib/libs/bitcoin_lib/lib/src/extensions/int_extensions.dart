a0b12ebf97b26f66614853638d529ede545535a8
hayato0311 <hayato8088@gmail.com>
2023-02-02 23:17:17 +0900

    add validation and error message

--- lib/libs/bitcoin_lib/lib/src/extensions/int_extensions.dart
+++ lib/libs/bitcoin_lib/lib/src/extensions/int_extensions.dart
@@ -1,58 +1,152 @@
+import 'dart:math';
 import 'dart:typed_data';
 
 extension IntExtensions on int {
   // of 1 byte
-  Uint8List toInt8Bytes() => Uint8List(1)..buffer.asByteData().setInt8(0, this);
+  Uint8List toInt8Bytes() {
+    if (this >= pow(2, 7)) {
+      throw RangeError('the value must be less than ${pow(2, 7)}(2^7)');
+    }
+    if (this < -pow(2, 7)) {
+      throw RangeError('the value must be ${-pow(2, 7)}(- 2^7) or more');
+    }
 
-  Uint8List toUint8Bytes() =>
-      Uint8List(1)..buffer.asByteData().setUint8(0, this);
+    return Uint8List(1)..buffer.asByteData().setInt8(0, this);
+  }
+
+  Uint8List toUint8Bytes() {
+    if (this >= pow(2, 8)) {
+      throw RangeError('the value must be less than ${pow(2, 8)}(2^8)');
+    }
+    if (this < 0) {
+      throw RangeError('the value must be 0 or more');
+    }
+
+    return Uint8List(1)..buffer.asByteData().setUint8(0, this);
+  }
 
   // int big endian
-  Uint8List toInt16beBytes() =>
-      Uint8List(2)..buffer.asByteData().setInt16(0, this);
+  Uint8List toInt16beBytes() {
+    if (this >= pow(2, 15)) {
+      throw RangeError('the value must be less than ${pow(2, 15)}(2^15)');
+    }
+    if (this < -pow(2, 15)) {
+      throw RangeError('the value must be ${-pow(2, 15)}(- 2^15) or more');
+    }
 
-  Uint8List toInt32beBytes() =>
-      Uint8List(4)..buffer.asByteData().setInt32(0, this);
+    return Uint8List(2)..buffer.asByteData().setInt16(0, this);
+  }
 
-  Uint8List toInt64beBytes() =>
-      Uint8List(8)..buffer.asByteData().setInt64(0, this);
+  Uint8List toInt32beBytes() {
+    if (this >= pow(2, 31)) {
+      throw RangeError('the value must be less than ${pow(2, 31)}(2^31)');
+    }
+    if (this < -pow(2, 31)) {
+      throw RangeError('the value must be ${-pow(2, 31)}(- 2^31) or more');
+    }
+
+    return Uint8List(4)..buffer.asByteData().setInt32(0, this);
+  }
+
+  Uint8List toInt64beBytes() {
+    return Uint8List(8)..buffer.asByteData().setInt64(0, this);
+  }
 
   // uint big endian
-  Uint8List toUint16beBytes() =>
-      Uint8List(2)..buffer.asByteData().setUint16(0, this);
+  Uint8List toUint16beBytes() {
+    if (this >= pow(2, 16)) {
+      throw RangeError('the value must be less than ${pow(2, 16)}(2^16)');
+    }
+    if (this < 0) {
+      throw RangeError('the value must be 0 or more');
+    }
 
-  Uint8List toUint32beBytes() =>
-      Uint8List(4)..buffer.asByteData().setUint32(0, this);
+    return Uint8List(2)..buffer.asByteData().setUint16(0, this);
+  }
 
-  Uint8List toUint64beBytes() =>
-      Uint8List(8)..buffer.asByteData().setUint64(0, this);
+  Uint8List toUint32beBytes() {
+    if (this >= pow(2, 32)) {
+      throw RangeError('the value must be less than ${pow(2, 32)}(2^32)');
+    }
+    if (this < 0) {
+      throw RangeError('the value must be 0 or more');
+    }
+
+    return Uint8List(4)..buffer.asByteData().setUint32(0, this);
+  }
+
+  Uint8List toUint64beBytes() {
+    if (this < 0) {
+      throw RangeError('the value must be 0 or more');
+    }
+
+    return Uint8List(8)..buffer.asByteData().setUint64(0, this);
+  }
 
   // int little endian
-  Uint8List toInt16leBytes() =>
-      Uint8List(2)..buffer.asByteData().setInt16(0, this, Endian.little);
+  Uint8List toInt16leBytes() {
+    if (this >= pow(2, 15)) {
+      throw RangeError('the value must be less than ${pow(2, 15)}(2^15)');
+    }
+    if (this < -pow(2, 15)) {
+      throw RangeError('the value must be ${-pow(2, 15)}(- 2^15) or more');
+    }
 
-  Uint8List toInt32leBytes() =>
-      Uint8List(4)..buffer.asByteData().setInt32(0, this, Endian.little);
+    return Uint8List(2)..buffer.asByteData().setInt16(0, this, Endian.little);
+  }
 
-  Uint8List toInt64leBytes() =>
-      Uint8List(8)..buffer.asByteData().setInt64(0, this, Endian.little);
+  Uint8List toInt32leBytes() {
+    if (this >= pow(2, 31)) {
+      throw RangeError('the value must be less than ${pow(2, 31)}(2^31)');
+    }
+    if (this < -pow(2, 31)) {
+      throw RangeError('the value must be ${-pow(2, 31)}(- 2^31) or more');
+    }
+
+    return Uint8List(4)..buffer.asByteData().setInt32(0, this, Endian.little);
+  }
+
+  Uint8List toInt64leBytes() {
+    return Uint8List(8)..buffer.asByteData().setInt64(0, this, Endian.little);
+  }
 
   // uint little endian
-  Uint8List toUint16leBytes() =>
-      Uint8List(2)..buffer.asByteData().setUint16(0, this, Endian.little);
+  Uint8List toUint16leBytes() {
+    if (this >= pow(2, 16)) {
+      throw RangeError('the value must be less than ${pow(2, 16)}(2^16)');
+    }
+    if (this < 0) {
+      throw RangeError('the value must be 0 or more');
+    }
 
-  Uint8List toUint32leBytes() =>
-      Uint8List(4)..buffer.asByteData().setUint32(0, this, Endian.little);
+    return Uint8List(2)..buffer.asByteData().setUint16(0, this, Endian.little);
+  }
 
-  Uint8List toUint64leBytes() =>
-      Uint8List(8)..buffer.asByteData().setUint64(0, this, Endian.little);
+  Uint8List toUint32leBytes() {
+    if (this >= pow(2, 32)) {
+      throw RangeError('the value must be less than ${pow(2, 32)}(2^32)');
+    }
+    if (this < 0) {
+      throw RangeError('the value must be 0 or more');
+    }
+
+    return Uint8List(4)..buffer.asByteData().setUint32(0, this, Endian.little);
+  }
+
+  Uint8List toUint64leBytes() {
+    if (this < 0) {
+      throw RangeError('the value must be 0 or more');
+    }
+
+    return Uint8List(8)..buffer.asByteData().setUint64(0, this, Endian.little);
+  }
 }
 
 extension CreateInt on int {
   // of 1 byte
   static int fromInt8Bytes(Uint8List bytes) {
     if (bytes.length != 1) {
-      throw ArgumentError();
+      throw ArgumentError('the length of bytes must be 1');
     }
 
     return bytes.buffer.asByteData().getInt8(0);
@@ -60,7 +154,7 @@ extension CreateInt on int {
 
   static int fromUint8Bytes(Uint8List bytes) {
     if (bytes.length != 1) {
-      throw ArgumentError();
+      throw ArgumentError('the length of bytes must be 1');
     }
 
     return bytes.buffer.asByteData().getUint8(0);
@@ -69,7 +163,7 @@ extension CreateInt on int {
   // int big endian
   static int fromInt16beBytes(Uint8List bytes) {
     if (bytes.length != 2) {
-      throw ArgumentError();
+      throw ArgumentError('the length of bytes must be 2');
     }
 
     return bytes.buffer.asByteData().getInt16(0);
@@ -77,7 +171,7 @@ extension CreateInt on int {
 
   static int fromInt32beBytes(Uint8List bytes) {
     if (bytes.length != 4) {
-      throw ArgumentError();
+      throw ArgumentError('the length of bytes must be 4');
     }
 
     return bytes.buffer.asByteData().getInt32(0);
@@ -85,7 +179,7 @@ extension CreateInt on int {
 
   static int fromInt64beBytes(Uint8List bytes) {
     if (bytes.length != 8) {
-      throw ArgumentError();
+      throw ArgumentError('the length of bytes must be 8');
     }
 
     return bytes.buffer.asByteData().getInt64(0);
@@ -94,7 +188,7 @@ extension CreateInt on int {
   // uint big endian
   static int fromUint16beBytes(Uint8List bytes) {
     if (bytes.length != 2) {
-      throw ArgumentError();
+      throw ArgumentError('the length of bytes must be 2');
     }
 
     return bytes.buffer.asByteData().getUint16(0);
@@ -102,7 +196,7 @@ extension CreateInt on int {
 
   static int fromUint32beBytes(Uint8List bytes) {
     if (bytes.length != 4) {
-      throw ArgumentError();
+      throw ArgumentError('the length of bytes must be 4');
     }
 
     return bytes.buffer.asByteData().getUint32(0);
@@ -111,7 +205,7 @@ extension CreateInt on int {
   // int little endian
   static int fromInt16leBytes(Uint8List bytes) {
     if (bytes.length != 2) {
-      throw ArgumentError();
+      throw ArgumentError('the length of bytes must be 2');
     }
 
     return bytes.buffer.asByteData().getInt16(0, Endian.little);
@@ -119,7 +213,7 @@ extension CreateInt on int {
 
   static int fromInt32leBytes(Uint8List bytes) {
     if (bytes.length != 4) {
-      throw ArgumentError();
+      throw ArgumentError('the length of bytes must be 4');
     }
 
     return bytes.buffer.asByteData().getInt32(0, Endian.little);
@@ -127,7 +221,7 @@ extension CreateInt on int {
 
   static int fromInt64leBytes(Uint8List bytes) {
     if (bytes.length != 8) {
-      throw ArgumentError();
+      throw ArgumentError('the length of bytes must be 8');
     }
 
     return bytes.buffer.asByteData().getInt64(0, Endian.little);
@@ -136,7 +230,7 @@ extension CreateInt on int {
   // uint little endian
   static int fromUint16leBytes(Uint8List bytes) {
     if (bytes.length != 2) {
-      throw ArgumentError();
+      throw ArgumentError('the length of bytes must be 2');
     }
 
     return bytes.buffer.asByteData().getUint16(0, Endian.little);
@@ -144,7 +238,7 @@ extension CreateInt on int {
 
   static int fromUint32leBytes(Uint8List bytes) {
     if (bytes.length != 4) {
-      throw ArgumentError();
+      throw ArgumentError('the length of bytes must be 4');
     }
 
     return bytes.buffer.asByteData().getUint32(0, Endian.little);
--- test/libs/bitcoin_lib/unit/extensions/int_extentions_test.dart
+++ test/libs/bitcoin_lib/unit/extensions/int_extentions_test.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'dart:typed_data';
 
 import 'package:flutter_test/flutter_test.dart';
@@ -7,52 +8,85 @@ void main() {
   group('convert integer into bytes', () {
     const value = 10;
     test('of 1 byte', () {
+      final invalidValue = pow(2, 8) as int;
+
       expect(value.toInt8Bytes(), Uint8List.fromList([10]));
       expect(value.toUint8Bytes(), Uint8List.fromList([10]));
+
+      expect(invalidValue.toInt8Bytes, throwsRangeError);
+      expect(invalidValue.toUint8Bytes, throwsRangeError);
     });
     group('as big endian', () {
       test('of int', () {
+        final invalidValue1 = pow(2, 16) as int;
+        final invalidValue2 = pow(2, 32) as int;
+
         expect(value.toInt16beBytes(), Uint8List.fromList([0, 10]));
         expect(value.toInt32beBytes(), Uint8List.fromList([0, 0, 0, 10]));
         expect(
           value.toInt64beBytes(),
           Uint8List.fromList([0, 0, 0, 0, 0, 0, 0, 10]),
         );
+
+        expect(invalidValue1.toInt16beBytes, throwsRangeError);
+        expect(invalidValue2.toInt32beBytes, throwsRangeError);
       });
       test('of uint', () {
+        final invalidValue1 = pow(2, 16) as int;
+        final invalidValue2 = pow(2, 32) as int;
+
         expect(value.toUint16beBytes(), Uint8List.fromList([0, 10]));
         expect(value.toUint32beBytes(), Uint8List.fromList([0, 0, 0, 10]));
         expect(
           value.toUint64beBytes(),
           Uint8List.fromList([0, 0, 0, 0, 0, 0, 0, 10]),
         );
+
+        expect(invalidValue1.toInt16beBytes, throwsRangeError);
+        expect(invalidValue2.toInt32beBytes, throwsRangeError);
       });
     });
     group('as little endian', () {
       test('of int', () {
+        final invalidValue1 = pow(2, 16) as int;
+        final invalidValue2 = pow(2, 32) as int;
+
         expect(value.toInt16leBytes(), Uint8List.fromList([10, 0]));
         expect(value.toInt32leBytes(), Uint8List.fromList([10, 0, 0, 0]));
         expect(
           value.toInt64leBytes(),
           Uint8List.fromList([10, 0, 0, 0, 0, 0, 0, 0]),
         );
+
+        expect(invalidValue1.toInt16beBytes, throwsRangeError);
+        expect(invalidValue2.toInt32beBytes, throwsRangeError);
       });
       test('of uint', () {
+        final invalidValue1 = pow(2, 16) as int;
+        final invalidValue2 = pow(2, 32) as int;
+
         expect(value.toUint16leBytes(), Uint8List.fromList([10, 0]));
         expect(value.toUint32leBytes(), Uint8List.fromList([10, 0, 0, 0]));
         expect(
           value.toUint64leBytes(),
           Uint8List.fromList([10, 0, 0, 0, 0, 0, 0, 0]),
         );
+
+        expect(invalidValue1.toInt16beBytes, throwsRangeError);
+        expect(invalidValue2.toInt32beBytes, throwsRangeError);
       });
     });
   });
 
   group('convert bytes into int', () {
     test('of 1 byte', () {
-      final bytes = Uint8List.fromList([10]);
-      expect(CreateInt.fromInt8Bytes(bytes), 10);
-      expect(CreateInt.fromUint8Bytes(bytes), 10);
+      final bytes1 = Uint8List.fromList([10]);
+      final bytes2 = Uint8List.fromList([10, 10]);
+      expect(CreateInt.fromInt8Bytes(bytes1), 10);
+      expect(CreateInt.fromUint8Bytes(bytes1), 10);
+
+      expect(() => CreateInt.fromInt8Bytes(bytes2), throwsArgumentError);
+      expect(() => CreateInt.fromUint8Bytes(bytes2), throwsArgumentError);
     });
     test('of 2 bytes', () {
       final bytes1 = Uint8List.fromList([0, 128]);
