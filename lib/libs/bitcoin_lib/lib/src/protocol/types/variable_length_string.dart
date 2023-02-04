71aa82cae5bab0cc4648277100e1b4ce3934a4b6
hayato0311 <hayato8088@gmail.com>
2023-02-04 09:43:55 +0900

    add deserialize function to VarStr

--- lib/libs/bitcoin_lib/lib/src/protocol/types/variable_length_string.dart
+++ lib/libs/bitcoin_lib/lib/src/protocol/types/variable_length_string.dart
@@ -7,6 +7,30 @@ class VarStr {
   VarStr(this.string) {
     length = VarInt(string.length);
   }
+
+  factory VarStr.deserialize(Uint8List bytes) {
+    final headByte = bytes[0];
+    final varIntBytesLength = VarInt.bytesLength(headByte);
+
+    if (bytes.length < varIntBytesLength) {
+      throw ArgumentError('The length of given bytes is invalid');
+    }
+
+    final length = VarInt.deserialize(bytes.sublist(0, varIntBytesLength));
+
+    final stringBytes = bytes.sublist(varIntBytesLength);
+
+    if (stringBytes.length != length.value) {
+      throw ArgumentError('The given bytes is invalid');
+    }
+
+    return VarStr(utf8.decode(stringBytes));
+  }
+
+  static int bytesLength(VarInt varInt) {
+    return 1 + varInt.length + varInt.value;
+  }
+
   late final VarInt length;
   final String string;
 
--- test/libs/bitcoin_lib/unit/protocol/types/variable_length_string_test.dart
+++ test/libs/bitcoin_lib/unit/protocol/types/variable_length_string_test.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'dart:typed_data';
 
 import 'package:flutter_test/flutter_test.dart';
+import 'package:reliet/libs/bitcoin_lib/lib/src/protocol/types/variable_length_integer.dart';
 import 'package:reliet/libs/bitcoin_lib/lib/src/protocol/types/variable_length_string.dart';
 
 void main() {
@@ -19,6 +20,31 @@ void main() {
         varStr.serialize(),
         Uint8List.fromList([0xfc, ...utf8.encode(string)]),
       );
+    });
+  });
+  group('deserialize bytes to VarStr instance', () {
+    test('with valid bytes', () {
+      const varStrValue = 'valid';
+      final serializedVarStrBytes = VarStr(varStrValue).serialize();
+      print(serializedVarStrBytes);
+
+      expect(VarStr.deserialize(serializedVarStrBytes).string, varStrValue);
+    });
+
+    test('with invalid bytes', () {
+      expect(
+        () => VarStr.deserialize(Uint8List.fromList([0, 0, 0, 1])),
+        throwsArgumentError,
+      );
+    });
+  });
+
+  group('get bytes of length based on varInt', () {
+    test('with valid varInt', () {
+      const varStrLength = 0xfd;
+      final varInt = VarInt(varStrLength);
+
+      expect(VarStr.bytesLength(varInt), 1 + varInt.length + varInt.value);
     });
   });
 }
