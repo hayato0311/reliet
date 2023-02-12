2393bbd660ee43c8b3f2eff30e92e84d1aed71c2
hayato0311 <hayato8088@gmail.com>
2023-02-12 19:01:24 +0900

    change message

--- lib/libs/bitcoin_lib/lib/src/spv_client.dart
+++ lib/libs/bitcoin_lib/lib/src/spv_client.dart
@@ -55,7 +55,7 @@ class PingObserver extends ProviderObserver {
 
     if (pingObserverState == null) return;
 
-    await sendPingMessage(pingObserverState.socket);
+    await sendPongMessage(pingObserverState.socket, pingObserverState.nonce);
   }
 }
 
