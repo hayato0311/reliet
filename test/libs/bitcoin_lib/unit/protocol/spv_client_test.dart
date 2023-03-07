6adeefbd55041e587629c5d79675c151003c7ef6
hayato0311 <hayato8088@gmail.com>
2023-03-07 09:09:31 +0900

    add optional nodeHost args to SpvClient

--- test/libs/bitcoin_lib/unit/protocol/spv_client_test.dart
+++ test/libs/bitcoin_lib/unit/protocol/spv_client_test.dart
@@ -4,7 +4,8 @@ import 'package:reliet/libs/bitcoin_lib/lib/src/spv_client.dart';
 void main() {
   // TODO: refactor to use mockito
   test('send ping, then recieve pong', () async {
-    final spvClient = SpvClient();
+    final spvClient =
+        SpvClient(nodeHost: '2406:da12:ce1:f001:1312:e40f:3d0f:2ebe');
 
     expect(
       spvClient.pongMessageRecieved,
