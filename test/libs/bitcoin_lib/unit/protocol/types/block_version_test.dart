69c6183d1d3f1b6ddbc4de6cf3d8f0ffd1978a40
hayato0311 <hayato8088@gmail.com>
2023-05-06 12:55:39 +0900

    add test for BlockVersion class

---
+++ test/libs/bitcoin_lib/unit/protocol/types/block_version_test.dart
@@ -0,0 +1,47 @@
+import 'dart:typed_data';
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:reliet/libs/bitcoin_lib/lib/src/extensions/int_extensions.dart';
+import 'package:reliet/libs/bitcoin_lib/lib/src/protocol/types/block_version.dart';
+
+void main() {
+  group('create and serialize BlockVersion instance', () {
+    test('with valid params', () {
+      const value = 1;
+      const blockVersion = BlockVersion(value);
+
+      expect(
+        blockVersion.serialize(),
+        value.toUint32leBytes(),
+      );
+    });
+    test('with invalid params', () {
+      const value = 3;
+      expect(
+        () => const BlockVersion(value),
+        throwsRangeError,
+      );
+    });
+  });
+  group('deserialize bytes to BlockVersion instance', () {
+    test('with valid bytes', () {
+      const value = 1;
+      const blockVersion = BlockVersion(value);
+
+      final serializedBlockVersion = blockVersion.serialize();
+
+      expect(
+        BlockVersion.deserialize(serializedBlockVersion),
+        isA<BlockVersion>(),
+      );
+    });
+    test('with invalid bytes', () {
+      expect(
+        () => BlockVersion.deserialize(
+          Uint8List.fromList([0, 0, 0, 1]),
+        ),
+        throwsArgumentError,
+      );
+    });
+  });
+}
