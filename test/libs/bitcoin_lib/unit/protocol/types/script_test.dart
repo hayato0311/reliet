f443848b5290dcb24806e812b45a4a7034619edb
hayato0311 <hayato8088@gmail.com>
2023-09-24 15:05:32 +0900

    add getPubkeyHash method to Script and fix serializeCommands method to static method

--- test/libs/bitcoin_lib/unit/protocol/types/script_test.dart
+++ test/libs/bitcoin_lib/unit/protocol/types/script_test.dart
@@ -3,6 +3,7 @@ import 'dart:typed_data';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:reliet/libs/bitcoin_lib/lib/src/protocol/types/op_code.dart';
 import 'package:reliet/libs/bitcoin_lib/lib/src/protocol/types/script.dart';
+import 'package:reliet/libs/bitcoin_lib/lib/src/protocol/types/script_type.dart';
 import 'package:reliet/libs/bitcoin_lib/lib/src/protocol/types/variable_length_integer.dart';
 
 void main() {
@@ -45,6 +46,47 @@ void main() {
       expect(deserializedScript.length.value, length.value);
       expect(deserializedScript.type, null);
       expect(deserializedScript.commands, commands);
+    });
+  });
+
+  group('get PubkeyHash', () {
+    test('from a script of type P2PKH', () {
+      final pubKeyHash = List<int>.filled(20, 10);
+      final commands = [
+        OpCode.opDup,
+        OpCode.opHash160,
+        OpCode.opPushBytes20,
+        pubKeyHash,
+        OpCode.opEqualVerify,
+        OpCode.opCheckSig
+      ];
+      final length = VarInt(25);
+      final script = Script(length, commands, ScriptType.p2pkh);
+
+      expect(script.getPubkeyHash(), pubKeyHash);
+    });
+  });
+
+  group('serialize commands', () {
+    test('of type P2PKH', () {
+      final pubKeyHash = List<int>.filled(20, 10);
+      final commands = [
+        OpCode.opDup,
+        OpCode.opHash160,
+        OpCode.opPushBytes20,
+        pubKeyHash,
+        OpCode.opEqualVerify,
+        OpCode.opCheckSig
+      ];
+
+      expect(Script.serializeCommands(commands), [
+        OpCode.opDup.value,
+        OpCode.opHash160.value,
+        OpCode.opPushBytes20.value,
+        ...pubKeyHash,
+        OpCode.opEqualVerify.value,
+        OpCode.opCheckSig.value
+      ]);
     });
   });
 }
